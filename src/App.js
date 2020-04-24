ef67c7ae64cc6fe6f328a929892637c8bbd57cf5
madu munachi <madustanley1@gmail.com>
2020-04-24 21:39:17 +0100

    redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux'
+
 import Post from './Post';
 import PostForm from './PostForm';
 
+
+import store from './store'
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">    
       <PostForm />
       <hr />
       <Post />
     </div>
+    </Provider>
   );
 }
 
--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,10 @@
 import React from 'react'
 
 class Post extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: []
-        }
-    }
+    
+    
 
-    componentWillMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => this.setState({posts: data}));
-
-    }
+    
 
     render() {
         const postItems = this.state.posts.map(post => (
